5b2adef0fe6c1036ff48425a707bbaa86e4fc66c
clhchtcjj <15720622991@163.com>
2018-08-01 08:34:57 +0800

    add link prediction

--- model/train.py
+++ model/train.py
@@ -11,6 +11,11 @@ from graph_utils import GraphUtils
 import random
 import math
 import os
+import pandas as pd
+from sklearn import metrics
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import average_precision_score,auc,precision_recall_fscore_support
 
 
 def init_embedding_vectors(node_u, node_v, node_list_u, node_list_v, args):
@@ -180,7 +185,7 @@ def top_N(test_u, test_v, test_rate, node_list_u, node_list_v, top_n):
 
         for (item, rate) in tmp_t:
             tmp_t_list.append(item)
-        pre, rec = precision_and_racall(tmp_r_list,tmp_t_list)
+        pre, rec = precision_and_recall(tmp_r_list,tmp_t_list)
         ap = AP(tmp_r_list,tmp_t_list)
         rr = RR(tmp_r_list,tmp_t_list)
         ndcg = nDCG(tmp_r_list,tmp_t_list)
@@ -235,7 +240,7 @@ def RR(ranked_list, ground_list):
             return 1 / (i + 1.0)
     return 0
 
-def precision_and_racall(ranked_list,ground_list):
+def precision_and_recall(ranked_list,ground_list):
     hits = 0
     for i in range(len(ranked_list)):
         id = ranked_list[i]
@@ -245,36 +250,57 @@ def precision_and_racall(ranked_list,ground_list):
     rec = hits/(1.0 * len(ground_list))
     return pre, rec
 
-def pre_train(node_list_u, node_list_v,edge_list,edge_dict_u, args):
-    alpha, beta, gamma, lam = args.alpha, args.beta, args.gamma, args.lam
-    last_loss = 0
-    epsilon = 1e-3
-    for iter in range(0, 100):
-        s1 = "\r[%s%s]%0.2f%%"%("*"* iter," "*(100-iter),iter*100.0/(args.max_iter-1))
-        loss = 0
-        num = 0
-        for (u, v, w) in edge_list:
-            update_u, update_v, tmp_loss = KL_divergence(edge_dict_u, u, v, node_list_u, node_list_v, lam, gamma)
-            loss += tmp_loss
-            node_list_u[u]['embedding_vectors'] += update_u
-            node_list_v[v]['embedding_vectors'] += update_v
-            num += 1
-        delt_loss = abs(loss - last_loss)
-        if last_loss > loss:
-            lam *= 1.05
-        else:
-            lam *= 0.95
-        last_loss = loss
-        if delt_loss < epsilon:
-            break
-        sys.stdout.write(s1)
-        sys.stdout.flush()
-    # with open(r"../data/puv.dat",'w') as fw:
-    #     for u,v,rate in edge_list:
-    #         U = node_list_u[u]['embedding_vectors']
-    #         V = node_list_v[v]['embedding_vectors']
-    #         X = float(U.dot(V.T))
-    #         fw.write(str(u)+"\t"+str(v)+"\t"+str(rate)+"\t"+str(X)+"\t"+str(1.0 / (1 + (math.exp(-X * 1.0))))+"\n")
+def generateFeatureFile(filecase,filevector_u,filevector_v,fileout,factors):
+    vectors_u = {}
+    vectors_v = {}
+    with open(filevector_u,'r') as fu:
+        for line in fu.readlines():
+            items = line.strip().split(' ')
+            vectors_u[items[0]] = items[1:]
+    with open(filevector_v,'r') as fv:
+        for line in fv.readlines():
+            items = line.strip().split(' ')
+            vectors_v[items[0]] = items[1:]
+    with open(filecase,'r') as fc, open(fileout,'w') as fo:
+        for line in fc.readlines():
+            items = line.strip().split('\t')
+            if vectors_u.get(items[0]) == None:
+                vectors_u[items[0]] = ['0'] * factors
+            if vectors_v.get(items[1]) == None:
+                vectors_v[items[1]] = ['0'] * factors
+            if items[-1] == '1':
+                fo.write('{}\t{}\t{}\n'.format('\t'.join(vectors_u[items[0]]),'\t'.join(vectors_v[items[1]]),1))
+            else:
+                fo.write('{}\t{}\t{}\n'.format('\t'.join(vectors_u[items[0]]),'\t'.join(vectors_v[items[1]]),0))
+
+def link_prediction(args):
+    filecase_a = args.case_train
+    filecase_e = args.case_test
+    filevector_u = args.vectors_u
+    filevector_v = args.vectors_v
+    filecase_a_c = r'../data/features_train.dat'
+    filecase_e_c = r'../data/features_test.dat'
+    generateFeatureFile(filecase_a,filevector_u,filevector_v,filecase_a_c,args.d)
+    generateFeatureFile(filecase_e,filevector_u,filevector_v,filecase_e_c,args.d)
+
+    df_data_train = pd.read_csv(filecase_a_c,header = None,sep='\t',encoding='utf-8')
+    X_train = df_data_train.drop(len(df_data_train.keys())-1,axis = 1)
+    y_train = df_data_train[len(df_data_train.keys())-1]
+
+    df_data_test = pd.read_csv(filecase_e_c,header = None,sep='\t',encoding='utf-8')
+    X_test = df_data_test.drop(len(df_data_train.keys())-1,axis = 1)
+    X_test = X_test.fillna(X_test.mean())
+    y_test = df_data_test[len(df_data_test.keys())-1]
+    y_test_list = list(y_test)
+
+    lg = LogisticRegression(penalty='l2',C=0.001)
+    lg.fit(X_train,y_train)
+    lg_y_pred_est = lg.predict_proba(X_test)[:,1]
+    fpr,tpr,thresholds = metrics.roc_curve(y_test,lg_y_pred_est)
+    average_precision = average_precision_score(y_test, lg_y_pred_est)
+    os.remove(filecase_a_c)
+    os.remove(filecase_e_c)
+    return metrics.auc(fpr,tpr), average_precision
 
 def train_by_sampling(args):
     model_path = os.path.join('../', args.model_name)
@@ -298,6 +324,7 @@ def train_by_sampling(args):
     node_list_u, node_list_v = {}, {}
     init_embedding_vectors(node_u, node_v, node_list_u, node_list_v, args)
     last_loss, count, epsilon = 0, 0, 1e-3
+ 
     print("============== training ==============")
     for iter in range(0, args.max_iter):
         s1 = "\r[%s%s]%0.2f%%"%("*"* iter," "*(args.max_iter-iter),iter*100.0/(args.max_iter-1))
@@ -359,7 +386,10 @@ def train_by_sampling(args):
         print("============== testing ===============")
         f1, map, mrr, mndcg = top_N(test_user,test_item,test_rate,node_list_u,node_list_v,args.top_n)
         print('recommendation metrics: F1 : %0.4f, MAP : %0.4f, MRR : %0.4f, NDCG : %0.4f' % (round(f1,4), round(map,4), round(mrr,4), round(mndcg,4)))
-
+    if args.lip:
+        print("============== testing ===============")
+        auc_roc, auc_pr = link_prediction(args)
+        print('link prediction metrics: AUC_ROC : %0.4f, AUC_PR : %0.4f' % (round(auc_roc,4), round(auc_pr,4)))
 
 def train(args):
     model_path = os.path.join('../', args.model_name)
@@ -383,8 +413,6 @@ def train(args):
     context_dict_u, neg_dict_u, context_dict_v, neg_dict_v, node_u, node_v = get_context_and_negative_samples(gul, args)
     node_list_u, node_list_v = {}, {}
     init_embedding_vectors(node_u, node_v, node_list_u, node_list_v, args)
-    # print("pretraining....")
-    # pre_train(node_list_u,node_list_v,edge_list,edge_dict_u,args)
 
     last_loss, count, epsilon = 0, 0, 1e-3
     print("============== training ==============")
@@ -446,6 +474,10 @@ def train(args):
         print("============== testing ===============")
         f1, map, mrr, mndcg = top_N(test_user,test_item,test_rate,node_list_u,node_list_v,args.top_n)
         print('recommendation metrics: F1 : %0.4f, MAP : %0.4f, MRR : %0.4f, NDCG : %0.4f' % (round(f1,4), round(map,4), round(mrr,4), round(mndcg,4)))
+    if args.lip:
+        print("============== testing ===============")
+        auc_roc, auc_pr = link_prediction(args)
+        print('link prediction metrics: AUC_ROC : %0.4f, AUC_PR : %0.4f' % (round(auc_roc,4), round(auc_pr,4)))
     
 
 
@@ -456,10 +488,10 @@ def ndarray_tostring(array):
     return string+"\n"
 
 def save_to_file(node_list_u,node_list_v,model_path,args):
-    with open(os.path.join(model_path,"vectors_u.dat"),"w") as fw_u:
+    with open(args.vectors_u,"w") as fw_u:
         for u in node_list_u.keys():
             fw_u.write(u+" "+ ndarray_tostring(node_list_u[u]['embedding_vectors']))
-    with open(os.path.join(model_path,"vectors_v.dat"),"w") as fw_v:
+    with open(args.vectors_v,"w") as fw_v:
         for v in node_list_v.keys():
             fw_v.write(v+" "+ndarray_tostring(node_list_v[v]['embedding_vectors']))
 
@@ -473,8 +505,21 @@ def main():
                         help='Input graph file.')
 
     parser.add_argument('--test-data', default=r'../data/rating_test.dat')
+
     parser.add_argument('--model-name', default='default',
                         help='name of model.')
+
+    parser.add_argument('--vectors-u', default=r'../data/vectors_u.dat',
+                        help="file of embedding vectors of U")
+
+    parser.add_argument('--vectors-v', default=r'../data/vectors_v.dat',
+                        help="file of embedding vectors of V")
+
+    parser.add_argument('--case-train', default=r'../data/wiki/case_train.dat',
+                        help="file of training data for LR")
+
+    parser.add_argument('--case-test', default=r'../data/wiki/case_test.dat',
+                        help="file of testing data for LR")
 
     parser.add_argument('--ws', default=5, type=int,
                         help='window size.')
@@ -511,8 +556,11 @@ def main():
     parser.add_argument('--top-n', default=10, type=int,
                         help='recommend top-n items for each user.')
 
-    parser.add_argument('--rec', default=1, type=int,
+    parser.add_argument('--rec', default=0, type=int,
                         help='calculate the recommendation metrics.')
+
+    parser.add_argument('--lip', default=0, type=int,
+                        help='calculate the link prediction metrics.')
 
     parser.add_argument('--large', default=0, type=int,
                         help='for large bipartite, 1 do not generate homogeneous graph file; 2 do not generate homogeneous graph')
